4bb2327c3144891da6159838bee6ca7b1186f757
The Mini John <admin@phoenix-prime.gr>
2014-07-18 20:00:50 +0300

    Remove coffee - add vanilla

---
+++ app/assets/javascripts/hydra.js
@@ -0,0 +1,13 @@
+window.Hydra = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Hydra.initialize();
+});
--- app/assets/javascripts/hydra.js.coffee
+++
@@ -1,9 +0,0 @@
-window.Hydra =
-  Models: {}
-  Collections: {}
-  Views: {}
-  Routers: {}
-  initialize: -> alert 'Hello from Backbone!'
-
-$(document).ready ->
-  Hydra.initialize()
