95654fa971b429ebf33d461757ccbb78f6865c90
The Mini John <admin@phoenix-prime.gr>
2014-07-18 18:39:33 +0300

    Turn React Addons on

---
+++ app/assets/javascripts/test.js.jsx
@@ -0,0 +1,6 @@
+// When you name your file with myfile.js.jsx, react-rails will automatically try
+// to transform that file. For the time being, we still require that you include the
+// docblock at the beginning of the file. For example, this file will get transformed
+// on request.
+
+/** @jsx React.DOM */
